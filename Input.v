3b32fc0020b8d9534b896acbccc4761f1965b7f1
liqiang <1391134476@qq.com>
2018-07-05 11:32:40 +0800

    输出作了修改（Value0-3改为了Value）

--- Input.v
+++ Input.v
@@ -1,27 +1,29 @@
 module Input(
-	input clk,
 	input rst,
 	input Left,
 	input Right,
 	input Up,
 	input Down,
 	input Enter,
-	output reg [3:0] Value0, // 归一化位移值十位
-	output reg [3:0] Value1, // 归一化位移值个位
-	output reg [3:0] Value2, // 归一化位移值小数点后一位
+	output reg [9:0] Value, // 归一化位移值
 	output reg [2:0] Motor, // 电机编号
 	output reg Lock // 电机锁
 	);
 	reg[1:0] Num; // 位号
-	always @(posedge clk or posedge rst)
+	reg[3:0] Value0;
+	reg[3:0] Value1;
+	reg[3:0] Value2;
+	always @(posedge rst or posedge Left or posedge Right 
+		or posedge Enter or posedge Up or posedge Down)
 	begin
 		if (rst) begin
-			Value0 = 4'b0000;
-			Value1 = 4'b0000;
-			Value2 = 4'b0000;
+			Value = 10'b00_0000_0000;
 			Motor = 3'b000;
 			Lock = 1'b0;
 			Num = 2'b00;
+			Value0 = 4'b0000;
+			Value1 = 4'b0000;
+			Value2 = 4'b0000;
 		end	
 		else begin
 			if (!Lock) begin // 选择电机
@@ -39,32 +41,22 @@ module Input(
 				if (Right) begin
 					Num = (Num==2'b10) ? Num+2'b10 : Num+2'b01;
 				end
+				
 				case(Num)
 					2'b00:begin
-						if (Down) begin
-							Value0 = !Value0 ? Value0-4'b0111 : Value0-4'b0001;
-						end
-						if (Up) begin
-							Value0 = (Value0==4'b1001) ? Value0+4'b0111 : Value0+4'b0001;
-						end
+							Value0 = Down ? (!Value0 ? Value0-4'b0111 : Value0-4'b0001) : Value0;
+							Value0 = Up ? ((Value0==4'b1001) ? Value0+4'b0111 : Value0+4'b0001) : Value0;
 					end
 					2'b01:begin
-						if (Down) begin
-							Value1 = !Value1 ? Value1-4'b0111 : Value1-4'b0001;
-						end
-						if (Up) begin
-							Value1 = (Value1==4'b1001) ? Value1+4'b0111 : Value1+4'b0001;
-						end
+							Value1 = Down ? (!Value1 ? Value1-4'b0111 : Value1-4'b0001) : Value1;
+							Value1 = Up ? ((Value1==4'b1001) ? Value1+4'b0111 : Value1+4'b0001) : Value1;
 					end
 					2'b10:begin
-						if (Down) begin
-							Value2 = !Value2 ? Value2-4'b0111 : Value2-4'b0001;
-						end
-						if (Up) begin
-							Value2 = (Value2==4'b1001) ? Value2+4'b0111 : Value2+4'b0001;
-						end
+							Value2 = Down ? (!Value2 ? Value2-4'b0111 : Value2-4'b0001) : Value2;
+							Value2 = Up ? ((Value2==4'b1001) ? Value2+4'b0111 : Value2+4'b0001) : Value2;
 					end
 				endcase
+				Value = Value0*100 + Value1*10 + Value2;
 			end
 			Lock = Enter ? ~Lock : Lock;
 		end
