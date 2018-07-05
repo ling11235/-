bc7db3ebf5b2ba30d479f3ca64551677223c80ab
liqiang <1391134476@qq.com>
2018-07-05 11:35:03 +0800

    更改了测试文件内容，增加了结果注释

--- Input_tb.v
+++ Input_tb.v
@@ -1,66 +1,64 @@
 `timescale 1ns/1ns
 
 module Input_tb;
-	reg rst, clk;
-	reg Left, Right, Enter, Up, Down;
-	wire[3:0] Value0;
-	wire[3:0] Value1;
-	wire[3:0] Value2;
-	wire[2:0] Motor;
+	reg rst, Left, Right, Enter, Up, Down;
+	wire[9:1'b0] Value;
+	wire[2:1'b0] Motor;
 	wire Lock;
 
 	initial begin
     	rst = 1'b0;
-    	clk = 1'b0;
     	Left = 1'b0;
     	Right = 1'b0;
     	Enter = 1'b0;
     	Up = 1'b0;
     	Down = 1'b0;
-    	#30 rst = 1;
-    	#10 rst = 0;
-    	#30 Right = 1;
-    	#10 Right = 0;
-    	#30 Right = 1;
-    	#10 Right = 0;
-    	#30 Right = 1;
-    	#10 Right = 0;
-    	#30 Right = 1;
-    	#10 Right = 0;
-    	#30 Right = 1;
-    	#10 Right = 0;
-    	#30 Left = 1;
-    	#10 Left = 0;
-    	#30 Left = 1;
-    	#10 Left = 0;
-    	#30 Left = 1;
-    	#10 Left = 0;
-    	#30 Enter = 1;
-    	#10 Enter = 0;
-    	#30 Right = 1;
-    	#10 Right = 0;
-    	#30 Right = 1;
-    	#10 Right = 0;
-    	#30 Up = 1;
-    	#10 Up = 0;
-    	#30 Up = 1;
-    	#10 Up = 0;
-    	#30 Down = 1;
-    	#10 Down = 0;
-    	#30 Down = 1;
-    	#10 Down = 0;
-    	#30 Down = 1;
-    	#10 Down = 0;
-    	#30 Down = 1;
-    	#10 Down = 0;
-    	#30 Enter = 1;
-    	#10 Enter = 0;
+    	#40 rst = 1'b1; // Value:0,Motor:0,Lock:0
+    	#10 rst = 1'b0;
+    	#40 Right = 1'b1; // Motor:1
+    	#10 Right = 1'b0;
+    	#40 Left = 1'b1; // Motor:0
+    	#10 Left = 1'b0;
+        #40 Left = 1'b1; // Motor:5
+        #10 Left = 1'b0;
+    	#40 Enter = 1'b1; // Lock:1
+    	#10 Enter = 1'b0;
+        #40 Up = 1'b1; // Value:100
+        #10 Up = 1'b0;
+        #40 Down = 1'b1; // Value:000
+        #10 Down = 1'b0;
+        #40 Down = 1'b1; // Value:900
+        #10 Down = 1'b0;
+    	#40 Right = 1'b1; // 十位
+    	#10 Right = 1'b0;
+        #40 Up = 1'b1; // Value:910
+        #10 Up = 1'b0;
+        #40 Down = 1'b1; // Value:900
+        #10 Down = 1'b0;
+        #40 Down = 1'b1; // Value:990
+        #10 Down = 1'b0;
+        #40 Right = 1'b1; // 个位
+        #10 Right = 1'b0;
+        #40 Up = 1'b1; // Value:991
+        #10 Up = 1'b0;
+        #40 Down = 1'b1; // Value:990
+        #10 Down = 1'b0;
+        #40 Down = 1'b1; // Value:999
+        #10 Down = 1'b0;
+        #40 Right = 1'b1; // 百位
+        #10 Right = 1'b0;
+        #40 Up = 1'b1; // Value:099
+        #10 Up = 1'b0;
+        #40 Down = 1'b1; // Value:999
+        #10 Down = 1'b0;
+        #40 Down = 1'b1; // Value:899
+        #10 Down = 1'b0;
+    	#40 Enter = 1'b1; // Lock:0
+    	#10 Enter = 1'b0;
 	end
-	always #5 clk = ~clk;
 
-	Input inputpart(.clk(clk),.rst(rst),
+	Input inputpart(.rst(rst),
 		.Left(Left),.Right(Right),.Enter(Enter),.Up(Up),.Down(Down),
-		.Value0(Value0),.Value1(Value1),.Value2(Value2),
-		.Motor(Motor),.Lock(Lock));
+		.Value(Value),.Motor(Motor),.Lock(Lock));
 
 endmodule
