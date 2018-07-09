bb5d2d581c4e87804b5558e18ead1a5113af8ded
liqiang <1391134476@qq.com>
2018-07-09 09:24:20 +0800

    去掉了Freqclk，修改了rst的触发沿，已仿真

--- PulseSign.v
+++ PulseSign.v
@@ -2,35 +2,28 @@
 module PulseSign(
 	input rst,
 	input clk,
+	input Motor,
 	input Enable, // 使能信号
 	input[9:0] PulseNum, // 脉冲数
 	output reg Sign // 脉冲信号
 	);
-	reg tmp;
-	reg Freqclk;
 	reg[9:0] cnt;
 	reg[14:0] Freqcnt;
 	
-	// 分频器，假设分频1/100
-	always @(posedge clk or posedge rst) begin
-		if (rst) begin
-			Freqclk = 0;
+	always @(posedge clk or negedge rst) begin
+		if (rst==1 || Enable==0) begin
 			Freqcnt = 0;
-		end
-		else begin // 可更改此处参数以改变频率
-			Freqclk <= Freqcnt==49 ? ~Freqclk : Freqclk;
-			Freqcnt <= Freqcnt==49 ? 0 : Freqcnt + 1;
-		end
-	end
-	always @(posedge Freqclk or posedge rst) begin
-		if (rst || !Enable) begin
 			Sign = 0;
 			cnt = 0;
-			tmp = 0;
 		end
-		else if (Enable) begin
-			Sign <= cnt<PulseNum ? ~Sign : 0;
-			cnt <= cnt<PulseNum ? (Sign ? cnt+1 : cnt) : cnt;
+		else if (Enable==1) begin // 可更改此处参数以改变频率 // 电机可接受脉冲频率的要求
+			if (Freqcnt==49) begin // 分频1/100
+				Freqcnt <= 0;
+				Sign <= cnt<PulseNum ? ~Sign : 0;
+				cnt <= cnt<PulseNum ? (Sign==1 ? cnt+1 : cnt) : cnt;
+			end
+			else Freqcnt <= Freqcnt + 1;
 		end
 	end
+
 endmodule
