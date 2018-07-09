dfddd1a2f640d4f7ca5b786f31d3fb2c67758df1
liqiang <1391134476@qq.com>
2018-07-09 15:32:46 +0800

    假定输出脉冲数等于位移数，已测试（见Control_tb.v）

--- Control.v
+++ Control.v
@@ -1,104 +1,65 @@
 // 控制模块，输出脉冲数、方向和上电信号。
-// 未测试，待改进。
 module Control(
 	input rst,
 	input clk,
 	input[5:0] InitFlag, // 初始化标志位
 	input[2:0] Motor, // 当前电机编号
 	input[9:0] Value, // 当前电机目标坐标
-	input Lock, // Input锁变量
-	input[5:0] Busy, // PulseSign模块标志位
-	output reg[5:0] PulseNums, // 电机脉冲数
-	output reg[5:0] DRs, // 方向信号
-	output reg[5:0] MFs // 上电信号，也即PulseSign模块的使能信号
+	input InputLock, // Input锁变量
+	input Busy, // PulseSign模块标志位
+	output reg[9:0] PulseNum, // 电机脉冲数
+	output reg Enable, // PulseSign模块的使能信号
+	output reg[5:0] DRs // 方向信号 // 1反转，0正传
 	);
-	reg[59:0] LastValues; // 所有电机上次坐标
 	reg[9:0] MotorValue; //当前电机归一化位移
-	reg[5:0] Pulsecache, // 电机脉冲数缓存
-	reg[5:0] DRcache, // 方向信号缓存
-	reg[5:0] MFcache // 上电信号缓存，也即PulseSign模块的使能信号缓存
+	reg[9:0] LastValues[5:0]; // 所有电机上次坐标
 
-	always @(posedge clk or posedge rst) begin
+	always @(posedge clk or negedge rst) begin
 		if (rst) begin
-			PUs = 0;
-			DRs = 0;
-			MFs = 0;
-			LastValues = 0;
+			PulseNum = 0; Enable = 0; DRs = 0;
 			MotorValue = 0;
+			LastValues[0] = 0; LastValues[1] = 0; LastValues[2] = 0;
+			LastValues[3] = 0; LastValues[4] = 0; LastValues[5] = 0;
 		end
-		else if (!Lock && InitFlag) begin
+		else if (InputLock==0 && InitFlag==6'b11_1111 && Busy==0) begin
 			// TODO:case语句实现归一化位移到脉冲数之间的转换
 			// MotorValue -> Pulsecache
 			// 计算当前电机的方向信号和位移值
 			case(Motor)
 				3'b000:begin
-					DRcache[0] = Value>LastValues[9:0] ? 1'b1:1'b0;
-					MotorValue = Value>LastValues[9:0] ? Value-LastValues[9:0] : LastValues[9:0]-Value;
-					MFcache[0] = 1'b1;
-					LastValues[9:0] = Value;
-
-					if (!Busy[0]) begin
-						DRs[0] = DRcache[0];
-						PulseNums[0] = Pulsecache[0];
-						MFs[0] = MFcache[0];
-					end
+					MotorValue <= Value<LastValues[0] ? LastValues[0]-Value : Value-LastValues[0];
+					DRs[0] <= Value<LastValues[0] ? 1 : (Value==LastValues[0] ?  DRs[0] : 0);
+					LastValues[0] <= Value;
 				end
 				3'b001:begin
-					DRcache[1] = Value>LastValues[19:10] ? 1'b1:1'b0;
-					MotorValue = Value>LastValues[19:10] ? Value-LastValues[19:10] : LastValues[19:10]-Value;
-					MFcache[1] = 1'b1;
-					LastValues[19:10] = Value;
-					if (!Busy[1]) begin
-						DRs[1] = DRcache[1];
-						PulseNums[1] = Pulsecache[1];
-						MFs[1] = MFcache[1];
-					end
+					MotorValue <= Value<LastValues[1] ? LastValues[1]-Value : Value-LastValues[1];
+					DRs[1] <= Value<LastValues[1] ? 1 : (Value==LastValues[1] ?  DRs[1] : 0);
+					LastValues[1] <= Value;
 				end
 				3'b010:begin
-					DRcache[2] = Value>LastValues[29:20] ? 1'b1:1'b0;
-					MotorValue = Value>LastValues[29:20] ? Value-LastValues[29:20] : LastValues[29:20]-Value;
-					MFcache[2] = 1'b1;
-					LastValues[29:20] = Value;
-					if (!Busy[2]) begin
-						DRs[2] = DRcache[2];
-						PulseNums[2] = Pulsecache[2];
-						MFs[2] = MFcache[2];
-					end
+					MotorValue <= Value<LastValues[2] ? LastValues[2]-Value : Value-LastValues[2];
+					DRs[2] <= Value<LastValues[2] ? 1 : (Value==LastValues[2] ?  DRs[2] : 0);
+					LastValues[2] <= Value;
 				end
 				3'b011:begin
-					DRcache[3] = Value>LastValues[39:30] ? 1'b1:1'b0;
-					MotorValue = Value>LastValues[39:30] ? Value-LastValues[39:30] : LastValues[39:30]-Value;
-					MFcache[3] = 1'b1;
-					LastValues[39:30] = Value;
-					if (!Busy[3]) begin
-						DRs[3] = DRcache[3];
-						PulseNums[3] = Pulsecache[3];
-						MFs[3] = MFcache[3];
-					end
+					MotorValue <= Value<LastValues[3] ? LastValues[3]-Value : Value-LastValues[3];
+					DRs[3] <= Value<LastValues[3] ? 1 : (Value==LastValues[3] ?  DRs[3] : 0);
+					LastValues[3] <= Value;
 				end
 				3'b100:begin
-					DRcache[4] = Value>LastValues[49:40] ? 1'b1:1'b0;
-					MotorValue = Value>LastValues[49:40] ? Value-LastValues[49:40] : LastValues[49:40]-Value;
-					MFcache[4] = 1'b1;
-					LastValues[49:40] = Value;
-					if (!Busy[4]) begin
-						DRs[4] = DRcache[4];
-						PulseNums[4] = Pulsecache[4];
-						MFs[4] = MFcache[4];
-					end
+					MotorValue <= Value<LastValues[4] ? LastValues[4]-Value : Value-LastValues[4];
+					DRs[4] <= Value<LastValues[4] ? 1 : (Value==LastValues[4] ?  DRs[4] : 0);
+					LastValues[4] <= Value;
 				end
 				3'b101:begin
-					DRcache[5] = Value>LastValues[59:50] ? 1'b1:1'b0;
-					MotorValue = Value>LastValues[59:50] ? Value-LastValues[59:50] : LastValues[59:50]-Value;
-					MFcache[5] = 1'b1;
-					LastValues[59:50] = Value;
-					if (!Busy[5]) begin
-						DRs[5] = DRcache[5];
-						PulseNums[5] = Pulsecache[5];
-						MFs[5] = MFcache[5];
-					end
+					MotorValue <= Value<LastValues[5] ? LastValues[5]-Value : Value-LastValues[5];
+					DRs[5] <= Value<LastValues[5] ? 1 : (Value==LastValues[5] ?  DRs[5] : 0);
+					LastValues[5] <= Value;
 				end
 			endcase
+			// 假设脉冲数等于位移数
+			PulseNum <= MotorValue==0 ? PulseNum : MotorValue;
+			Enable <= MotorValue==0 ? 1 : 0;
 		end
 	end
 endmodule
