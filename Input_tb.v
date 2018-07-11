48a1ef2fc08a47b6071ce03f7dfa9853966240a9
liqiang <1391134476@qq.com>
2018-07-11 15:12:12 +0800

    更新了测试内容

--- Input_tb.v
+++ Input_tb.v
@@ -1,64 +1,51 @@
 `timescale 1ns/1ns
-// Input模块测试文件
+// Input模块测试文档
 module Input_tb;
-	reg rst, Left, Right, Enter, Up, Down;
-	wire[9:1'b0] Value;
-	wire[2:1'b0] Motor;
-	wire Lock;
+	reg rst,sysclk;
+    reg Left, Right, Enter, Up, Down;
+	wire[11:0] Value;
+	wire[5:0] Motor;
 
 	initial begin
-    	rst = 1'b0;
-    	Left = 1'b0;
-    	Right = 1'b0;
-    	Enter = 1'b0;
-    	Up = 1'b0;
-    	Down = 1'b0;
-    	#40 rst = 1'b1; // Value:0,Motor:0,Lock:0
-    	#10 rst = 1'b0;
-    	#40 Right = 1'b1; // Motor:1
-    	#10 Right = 1'b0;
-    	#40 Left = 1'b1; // Motor:0
-    	#10 Left = 1'b0;
-        #40 Left = 1'b1; // Motor:5
-        #10 Left = 1'b0;
-    	#40 Enter = 1'b1; // Lock:1
-    	#10 Enter = 1'b0;
-        #40 Up = 1'b1; // Value:100
-        #10 Up = 1'b0;
-        #40 Down = 1'b1; // Value:000
-        #10 Down = 1'b0;
-        #40 Down = 1'b1; // Value:900
-        #10 Down = 1'b0;
-    	#40 Right = 1'b1; // 十位
-    	#10 Right = 1'b0;
-        #40 Up = 1'b1; // Value:910
-        #10 Up = 1'b0;
-        #40 Down = 1'b1; // Value:900
-        #10 Down = 1'b0;
-        #40 Down = 1'b1; // Value:990
-        #10 Down = 1'b0;
-        #40 Right = 1'b1; // 个位
-        #10 Right = 1'b0;
-        #40 Up = 1'b1; // Value:991
-        #10 Up = 1'b0;
-        #40 Down = 1'b1; // Value:990
-        #10 Down = 1'b0;
-        #40 Down = 1'b1; // Value:999
-        #10 Down = 1'b0;
-        #40 Right = 1'b1; // 百位
-        #10 Right = 1'b0;
-        #40 Up = 1'b1; // Value:099
-        #10 Up = 1'b0;
-        #40 Down = 1'b1; // Value:999
-        #10 Down = 1'b0;
-        #40 Down = 1'b1; // Value:899
-        #10 Down = 1'b0;
-    	#40 Enter = 1'b1; // Lock:0
-    	#10 Enter = 1'b0;
+    	rst = 1;
+        sysclk = 1;
+    	Left = 0;
+    	Right = 0;
+    	Enter = 0;
+    	Up = 0;
+    	Down = 0;
+    	#100 rst = 0; // 复位
+    	#100 rst = 1; // Motor:000000,MotorCache:000001,Num:00
+                      // Value0:0,Value1:0,Value2:0,Value:0000_0000_0000
+        #100 Down = 1;
+        #100 Down = 0; // MotorCache:100000
+        #100 Down = 1;
+        #100 Down = 0; // MotorCache:010000
+        #100 Left = 1;
+        #100 Left = 0; // Num:11
+        #100 Down = 1;
+        #100 Down = 0; // Value2:9
+        #100 Down = 1;
+        #100 Down = 0; // Value2:8
+        #100 Left = 1;
+        #100 Left = 0; // Num:10
+        #100 Down = 1;
+        #100 Down = 0; // Value1:9
+        #100 Down = 1;
+        #100 Down = 0; // Value1:8
+        #100 Left = 1;
+        #100 Left = 0; // Num:01
+        #100 Down = 1;
+        #100 Down = 0; // Value0:9
+        #100 Down = 1;
+        #100 Down = 0; // Value0:8
+        #100 Enter = 1;
+        #100 Enter = 0; // Motor:010000,Value:1000_1000_1000
 	end
 
-	Input inputpart(.rst(rst),
-		.Left(Left),.Right(Right),.Enter(Enter),.Up(Up),.Down(Down),
-		.Value(Value),.Motor(Motor),.Lock(Lock));
+    always #5 sysclk = ~sysclk;
 
+	Input inputpart(.rst(rst),.sysclk(sysclk),
+		.Left(Left),.Right(Right),.Up(Up),.Down(Down),.Enter(Enter),
+		.Value(Value),.Motor(Motor));
 endmodule
