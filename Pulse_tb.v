8703d44a2a3fccd71bc594d3836813ec9a391bcc
liqiang <1391134476@qq.com>
2018-07-12 22:57:19 +0800

    Update Pulse_tb.v

--- Pulse_tb.v
+++ Pulse_tb.v
@@ -7,12 +7,13 @@ reg[9:0] PulseNum;
 reg[5:0] Stop;
 wire Busy;
 wire[5:0] initFlag;
+wire SS,DSS;
 wire[5:0] PU;
 wire[5:0] MF;
 
 
 Pulse PS(.sysclk(sysclk),.Motor(Motor),.PulseNum(PulseNum),.Stop(Stop),
-		.Busy(Busy),.initFlag(initFlag),.PU(PU),.MF(MF));
+		.Busy(Busy),.initFlag(initFlag),.SS(SS),.DSS(DSS),.PU(PU),.MF(MF));
 
 initial begin
 	sysclk = 0;
@@ -20,17 +21,17 @@ initial begin
 	PulseNum = 0;
 	Stop = 0;
 	#10000 Stop = 6'b00_0001;
-	#1000 Stop = 0;
+	#2000 Stop = 0;
 	#10000 Stop = 6'b00_0010;
-	#1000 Stop = 0;
+	#2000 Stop = 0;
 	#10000 Stop = 6'b00_0100;
-	#1000 Stop = 0;
+	#2000 Stop = 0;
 	#10000 Stop = 6'b00_1000;
-	#1000 Stop = 0;
+	#2000 Stop = 0;
 	#10000 Stop = 6'b01_0000;
-	#1000 Stop = 0;
+	#2000 Stop = 0;
 	#10000 Stop = 6'b10_0000;
-	#1000 Stop = 0;         // 原点标定完毕
+	#2000 Stop = 0;         // 原点标定完毕
 	#10000 Motor = 6'b0_0010; PulseNum = 5;
 	#10000 PulseNum = 2;
 	#10000 Motor = 6'b0_1000; PulseNum = 5;
