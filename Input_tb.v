73f6f4d07045bf2a7629eb8868b0b9a556cd44c7
liqiang <1391134476@qq.com>
2018-07-12 23:07:55 +0800

    Update Input_tb.v

--- Input_tb.v
+++ Input_tb.v
@@ -3,7 +3,9 @@
 module Input_tb;
 	reg rst,sysclk;
     reg Left, Right, Enter, Up, Down;
-	wire[11:0] Value;
+	wire[3:0] TValue0;
+    wire[3:0] TValue1;
+    wire[3:0] TValue2;
 	wire[5:0] Motor;
 
 	initial begin
@@ -16,7 +18,7 @@ module Input_tb;
     	Down = 0;
     	#100 rst = 0; // 复位
     	#100 rst = 1; // Motor:000000,MotorCache:000001,Num:00
-                      // Value0:0,Value1:0,Value2:0,Value:0000_0000_0000
+                      // TValue0:0,TValue1:0,TValue2:0,
         #100 Down = 1;
         #100 Down = 0; // MotorCache:100000
         #100 Down = 1;
@@ -24,28 +26,28 @@ module Input_tb;
         #100 Left = 1;
         #100 Left = 0; // Num:11
         #100 Down = 1;
-        #100 Down = 0; // Value2:9
+        #100 Down = 0; // TValue2:9
         #100 Down = 1;
-        #100 Down = 0; // Value2:8
+        #100 Down = 0; // TValue2:8
         #100 Left = 1;
         #100 Left = 0; // Num:10
         #100 Down = 1;
-        #100 Down = 0; // Value1:9
+        #100 Down = 0; // TValue1:9
         #100 Down = 1;
-        #100 Down = 0; // Value1:8
+        #100 Down = 0; // TValue1:8
         #100 Left = 1;
         #100 Left = 0; // Num:01
         #100 Down = 1;
-        #100 Down = 0; // Value0:9
+        #100 Down = 0; // TValue0:9
         #100 Down = 1;
-        #100 Down = 0; // Value0:8
+        #100 Down = 0; // TValue0:8
         #100 Enter = 1;
-        #100 Enter = 0; // Motor:010000,Value:1000_1000_1000
+        #100 Enter = 0; // Motor:010000
 	end
 
     always #5 sysclk = ~sysclk;
 
 	Input inputpart(.rst(rst),.sysclk(sysclk),
 		.Left(Left),.Right(Right),.Up(Up),.Down(Down),.Enter(Enter),
-		.Value(Value),.Motor(Motor));
+		.TValue0(TValue0),.TValue1(TValue1),.TValue2(TValue2),.Motor(Motor));
 endmodule
