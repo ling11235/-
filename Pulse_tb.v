a8acc92fa6224a407f628b94b403f2b0dc0c1f59
liqiang <1391134476@qq.com>
2018-07-13 16:17:22 +0800

    Update Pulse_tb.v

--- Pulse_tb.v
+++ Pulse_tb.v
@@ -3,23 +3,24 @@
 module Pulse_tb;
 reg sysclk;
 reg[5:0] Motor;
-reg[9:0] PulseNum;
 reg[5:0] Stop;
+reg[5:0] DRIn;
+reg[9:0] PulseNum;
 wire Busy;
 wire[5:0] initFlag;
-wire SS,DSS;
 wire[5:0] PU;
 wire[5:0] MF;
+wire[5:0] DR;
 
-
-Pulse PS(.sysclk(sysclk),.Motor(Motor),.PulseNum(PulseNum),.Stop(Stop),
-		.Busy(Busy),.initFlag(initFlag),.SS(SS),.DSS(DSS),.PU(PU),.MF(MF));
+Pulse PS(.sysclk(sysclk),.Motor(Motor),.PulseNum(PulseNum),.DRIn(DRIn),.Stop(Stop),
+		.Busy(Busy),.INIT(INIT),.initFlag(initFlag),.PU(PU),.MF(MF),.DR(DR));
 
 initial begin
 	sysclk = 0;
 	Motor = 0;
 	PulseNum = 0;
 	Stop = 0;
+	DRIn = 0;
 	#10000 Stop = 6'b00_0001;
 	#2000 Stop = 0;
 	#10000 Stop = 6'b00_0010;
@@ -31,10 +32,11 @@ initial begin
 	#10000 Stop = 6'b01_0000;
 	#2000 Stop = 0;
 	#10000 Stop = 6'b10_0000;
-	#2000 Stop = 0;         // 原点标定完毕
-	#10000 Motor = 6'b0_0010; PulseNum = 5;
-	#10000 PulseNum = 2;
-	#10000 Motor = 6'b0_1000; PulseNum = 5;
+	#2000 Stop = 0;
+	// 原点标定完毕
+	#10000 Motor = 6'b0_0010; PulseNum = 5; DRIn = 6'b0_0000;
+	#10000 PulseNum = 2; DRIn = 6'b0_0010;
+	#10000 Motor = 6'b0_1000; PulseNum = 5; DRIn = 6'b0_0010;
 	
 end
 always #5 sysclk = ~sysclk; // 时钟周期10ns, 脉冲周期1000ns
