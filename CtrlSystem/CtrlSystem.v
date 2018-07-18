000e3d8c67512e95d6bbcee887d61bc8d9fb5af9
liqiang <1391134476@qq.com>
2018-07-18 11:15:10 +0800

    移除外部rst，并为LCD显示增加输出。

--- CtrlSystem/CtrlSystem.v
+++ CtrlSystem/CtrlSystem.v
@@ -1,7 +1,6 @@
 // 五键输入，产生电机的控制信号，初始化时标定原点。
 // 需要有传感器输入限位信号Stop。
 module CtrlSystem(
-	input rst,
 	input sysclk,
 	input Left,
 	input Right,
@@ -9,6 +8,9 @@ module CtrlSystem(
 	input Down,
 	input Enter,
 	input[5:0] Stop,
+	output wire[1:0] Num, // 位号
+	output wire LCD_Enable, // LCD使能信号
+	output wire[3:0] LCD_Num, // LCD显示数字
 	output wire[5:0] PU, // 脉冲
 	output wire[5:0] MF, // 上电
 	output wire[5:0] DR // 方向
@@ -18,13 +20,13 @@ module CtrlSystem(
 	wire[5:0] Motor, MotorOut, DROut, initFlag;
 	wire[9:0] PulseNum;
 
-	Input inputpart(.rst(rst),.sysclk(sysclk),.INIT(INIT),
+	Input inputpart(.sysclk(sysclk),.INIT(INIT),
 				.Left(Left),.Right(Right),.Up(Up),.Down(Down),.Enter(Enter),
 				.TValue0(TValue0),.TValue1(TValue1),.TValue2(TValue2),
-				.Motor(Motor));
+				.Motor(Motor),.Num(Num),.LCD_Enable(LCD_Enable),.LCD_Num(LCD_Num));
 
-	Control controlpart(.rst(rst),.sysclk(sysclk),
-				.initFlag(initFlag),.INIT(INIT),.Busy(Busy),
+	Control controlpart(.sysclk(sysclk),.INIT(INIT),
+				.initFlag(initFlag),.Busy(Busy),
 				.TValue0(TValue0),.TValue1(TValue1),.TValue2(TValue2),.Motor(Motor),
 				.MotorOut(MotorOut),.PulseNum(PulseNum),.DROut(DROut));
 
