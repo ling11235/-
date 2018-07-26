7949eafaae6071646e528744bc97866180c75ceb
liqiang <1391134476@qq.com>
2018-07-26 22:57:16 +0800

    删掉了多余的寄存器

--- TopModule/Control.v
+++ TopModule/Control.v
@@ -4,47 +4,36 @@ module Control #(parameter DATA_NUM = 1000, DATA_WIDTH = 10) (
 	input sysclk,
 	input[5:0] initFlag, // 初始化标志位
 	input INIT, // 初始化标识
-	input[3:0] i_Motor, // 电机编号 // 位数根据实际情况可以改
+	input[2:0] i_Motor, // 电机编号 // 位数根据实际情况可以改
 	input[9:0] Value, // 电机目标坐标
 	input Busy, // Pulse模块标志位
 	output reg[5:0] o_Motor, //输出电机号
 	output reg[DATA_WIDTH-1:0] PulseNum, // 电机脉冲数
-	output reg[5:0] DROut // 输入到Pulse的方向信号
+	output reg[5:0] DRSign // 方向信号: 1反转，0正传
 	);
 
 	(* ram_style = "block" *)
 	reg[DATA_WIDTH-1:0] datas[DATA_NUM-1:0];
 	initial $readmemh("data.txt", datas);
 
-	reg FLAG; // 读数据标志
-	reg [DATA_WIDTH-1:0] data_pulse; // 当前脉冲数
-
-	reg[2:0] Motor; //输入电机号
-	reg[5:0] DRSign; // 方向信号: 1反转，0正传
 	reg[9:0] LastValue[5:0];	// 所有电机上次地址
 	reg[9:0] DiffValue; //当前电机位移 // 数据地址
 
-	// Motor输入缓存
-	always @(posedge sysclk) begin
-		if (INIT==1) 
-			Motor <= 0;
-		else if (&initFlag==1 && Busy==0) // 标定好原点且Pulse空闲
-			Motor <= i_Motor[2:0];
-	end
 	// 方向信号
 	always @(posedge sysclk) begin
 		if (INIT==1) 
 			DRSign <= 0;
 		else if (&initFlag==1 && Busy==0)
-			DRSign[Motor] <= Value<LastValue[Motor] ? 1 : (Value==LastValue[Motor] ?  DRSign[Motor] : 0);
+			DRSign[i_Motor] <= Value<LastValue[i_Motor] ? 1 : (Value==LastValue[i_Motor] ?  DRSign[i_Motor] : 0);
 	end
 	// 位移值
 	always @(posedge sysclk) begin
 		if (INIT==1)
 			DiffValue <= 0;
 		else if (&initFlag==1 && Busy==0)
-			DiffValue <= Value<LastValue[Motor] ? LastValue[Motor]-Value : Value-LastValue[Motor];
+			DiffValue <= Value<LastValue[i_Motor] ? LastValue[i_Motor]-Value : Value-LastValue[i_Motor];
 	end
+	
 	// 更新坐标值
 	always @(posedge sysclk) begin
 		if (INIT==1) begin
@@ -52,38 +41,31 @@ module Control #(parameter DATA_NUM = 1000, DATA_WIDTH = 10) (
 			LastValue[3] <= 0; LastValue[4] <= 0; LastValue[5] <= 0;
 		end
 		else if (&initFlag==1 && Busy==0)
-			LastValue[Motor] <= Value;
+			LastValue[i_Motor] <= Value;
 	end
 
-	// 根据位移值读脉冲数
+	// 读脉冲数
 	always @(posedge sysclk) begin
 		if (INIT==1) begin
-			data_pulse <= 0; FLAG <= 0;
+			PulseNum <= 0;
+		end
+		else if (&initFlag==1 && Busy==0)
+			PulseNum <= DiffValue==0 ? PulseNum : datas[DiffValue];
+	end
+
+	// 输出电机标志
+	always @(posedge sysclk) begin
+		if (INIT) begin
+			o_Motor <= 0;
 		end
 		else if (&initFlag==1 && Busy==0) begin
-			data_pulse <= datas[DiffValue];
-			FLAG <= 1;
+			o_Motor[0] <= ~(i_Motor[2] | i_Motor[1] | i_Motor[0]);
+			o_Motor[1] <= ~(i_Motor[2] | i_Motor[1] | ~i_Motor[0]);
+			o_Motor[2] <= i_Motor[1] & ~i_Motor[0];
+			o_Motor[3] <= i_Motor[1] & i_Motor[0];
+			o_Motor[4] <= i_Motor[2] & ~i_Motor[0];
+			o_Motor[5] <= i_Motor[2] & i_Motor[0];	
 		end
-		else
-			FLAG <= 0;
 	end
-	// 取得数据后输出
-	always @(posedge sysclk) begin
-		if (INIT==1) begin
-			o_Motor <= 0;
-			PulseNum <= 0;
-			DROut <= 0;
-		end
-		else if (FLAG==1) begin
-			o_Motor[0] <= ~(Motor[2] | Motor[1] | Motor[0]);
-			o_Motor[1] <= ~(Motor[2] | Motor[1] | ~Motor[0]);
-			o_Motor[2] <= Motor[1] & ~Motor[0];
-			o_Motor[3] <= Motor[1] & Motor[0];
-			o_Motor[4] <= Motor[2] & ~Motor[0];
-			o_Motor[5] <= Motor[2] & Motor[0];
 
-			PulseNum <= data_pulse;
-			DROut <= DRSign;
-		end
-	end
 endmodule
